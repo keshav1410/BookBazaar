bc601e4aa825683fdade1b7722d7ec9b8dc62d3a
AdityaGlobalLogic <aditya.goyal@globallogic.com>
2023-05-10 12:00:18 +0530

    Crousal images updated and bug fixes

--- src/Components/InfoSection.js
+++ src/Components/InfoSection.js
@@ -4,33 +4,30 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import SkipNextIcon from "@mui/icons-material/SkipNext";
+
 import { Container, Grid } from "@mui/material";
 
 const data = [
   {
     title: "Free Shipping",
     desc: "Free Delivery on all orders",
-    cardImage: "/assets/Capture.png",
+    cardImage: "/assets/InfoSectionImages/Capture.png",
   },
   {
     title: "Money Guarantee",
     desc: "30 days money back",
-    cardImage: "/assets/Capture1.png",
+    cardImage: "/assets/InfoSectionImages/Capture1.png",
   },
   {
     title: "24/7 Support",
     desc: "Friendly 24/7 support",
-    cardImage: "/assets/Capture2.png",
+    cardImage: "/assets/InfoSectionImages/Capture2.png",
   },
   {
     title: "Secure Payment",
     desc: "All cards accepted",
-    cardImage: "/assets/Capture3.png",
+    cardImage: "/assets/InfoSectionImages/Capture3.png",
   },
 ];
 
